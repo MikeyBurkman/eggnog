f7f47cb804c1a8767c62ad6b8552a8409e19660e
Michael Burkman <michael.burkman@gmail.com>
2014-12-20 09:56:28 -0500

    minor clarification

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 
 var NOD = require('./index.js')();
 
-NOD.scanForFiles('./testapp');
+NOD.scanForFiles('./testapp'); // Load all files in ./testapp
 
+// This is the module to start in. Returns the return value of appstart.init()
 var startup = NOD.loadModule('appstart');
 console.log('Startup successful: ', startup);
